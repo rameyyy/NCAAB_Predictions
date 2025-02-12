1fee6841b83e834b79dc0a7635f592a7543b2f29
caramey <clayrameyy@gmail.com>
2025-02-12 02:54:50 -0600

    e

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import reportgen
 rg = reportgen
 rg.initialize_path(file_path_to_path)
 rg.CurrentDayScrape().scrape_all(True)
-rg.CurrentDayReport().generate_report(True, True, '02-11-2025_match_predictions.txt', True)
+rg.CurrentDayReport().generate_report(True, True, '02-12-2025_match_predictions.txt', True)
